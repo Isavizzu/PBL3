7ec2f821b14ccdd209509a5c4dc90a68a9d6a184
HelenaAlmeid <165021004+HelenaAlmeid@users.noreply.github.com>
2024-04-07 21:14:52 -0300

    Mudança

--- Passageiro.java
+++ Passageiro.java
@@ -1,8 +1,23 @@
+import java.util.ArrayList;
+
 public class Passageiro extends Usuario{
     private String enderecodecobranca;
+    private ArrayList<Endereco> enderecos;
 
     public Passageiro (String nome, String genero, String datadenascimento, String cpf, String enderecodecobranca){
         super(nome,genero,datadenascimento,cpf);
         this.enderecodecobranca = enderecodecobranca;
+        this.enderecos = new ArrayList<>();
+
     }
+
+    public ArrayList<Endereco> getEnderecos() {
+        return enderecos;
+    }
+    
+    public void adicionarEndereco(Endereco endereco) {
+        enderecos.add(endereco);
+    }
+    
+    
 }
